18c56e966300b83b44b483bdbb4971bd617722c5
JaeHyeokSeong <seong5763@gmail.com>
2024-04-04 19:21:47 +1100

    스코프와 프록시

--- 스프링핵심원리-기본편/core/src/main/java/hello/core/web/LogDemoController.java
+++ 스프링핵심원리-기본편/core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,21 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
-
-    @Autowired
-    public LogDemoController(LogDemoService logDemoService, ObjectProvider<MyLogger> myLogger) {
-        this.logDemoService = logDemoService;
-        this.myLoggerObjectProvider = myLogger;
-    }
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
--- 스프링핵심원리-기본편/core/src/main/java/hello/core/web/LogDemoService.java
+++ 스프링핵심원리-기본편/core/src/main/java/hello/core/web/LogDemoService.java
@@ -1,22 +1,18 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
-
-    @Autowired
-    public LogDemoService(ObjectProvider<MyLogger> myLoggerObjectProvider) {
-        this.myLoggerObjectProvider = myLoggerObjectProvider;
-    }
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
