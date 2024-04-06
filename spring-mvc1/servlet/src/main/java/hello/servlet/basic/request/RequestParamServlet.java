a53c8a6b8bf65b8d244b3abe8ea4bfdf2388ff03
JaeHyeokSeong <seong5763@gmail.com>
2024-04-06 12:15:23 +1100

    http 요청 데이터 - get 쿼리 파라미터

---
+++ spring-mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,47 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * http://127.0.0.1:8080/request-param?username=hello&age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while(parameterNames.hasMoreElements()) {
+            String parameterName = parameterNames.nextElement();
+            System.out.println(parameterName + "=" + request.getParameter(parameterName));
+        }
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        System.out.println("request.getParameterValues(username)");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username=" + name);
+        }
+        System.out.println();
+
+        response.getWriter().write("OK");
+    }
+}
