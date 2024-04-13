0bca98598871be7741b3a1704a54774d65436401
JaeHyeokSeong <seong5763@gmail.com>
2024-04-13 17:04:49 +1000

    상품 목록 - 타임리프

---
+++ spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,38 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/basic/items")
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트 목적으로 임의 값을 집어 넣음
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 20000, 20));
+        itemRepository.save(new Item("testC", 30000, 30));
+    }
+}
