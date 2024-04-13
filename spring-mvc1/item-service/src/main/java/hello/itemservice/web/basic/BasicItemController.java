ea610c2fc312a14ad81bdae459573e34467a57ac
JaeHyeokSeong <seong5763@gmail.com>
2024-04-13 20:36:16 +1000

    상품 등록 폼

--- spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -43,4 +43,9 @@ public class BasicItemController {
         model.addAttribute("item", item);
         return "basic/item";
     }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
 }
