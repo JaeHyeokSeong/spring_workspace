53d6bc3e014561d1578d9bc72778f1540ed87f7b
JaeHyeokSeong <seong5763@gmail.com>
2024-04-13 18:47:28 +1000

    상품 목록 - 타임리프

--- spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,13 +20,6 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
-    @GetMapping
-    public String items(Model model) {
-        List<Item> items = itemRepository.findAll();
-        model.addAttribute("items", items);
-        return "basic/items";
-    }
-
     /**
      * 테스트 목적으로 임의 값을 집어 넣음
      */
@@ -34,5 +28,19 @@ public class BasicItemController {
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
         itemRepository.save(new Item("testC", 30000, 30));
+    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 }
