d3078a2155bcd6d73c82ab7bfaff0e0565290ddd
JaeHyeokSeong <seong5763@gmail.com>
2024-04-23 02:06:02 +1000

    요구사항 추가

---
+++ spring-mvc2/form/src/main/java/hello/itemservice/enumPractise/Delivery.java
@@ -0,0 +1,17 @@
+package hello.itemservice.enumPractise;
+
+import lombok.Getter;
+
+@Getter
+public enum Delivery {
+
+//    FAST("빠른배송"), STANDARD("기본배송");
+//
+//    private final String description;
+//
+//    private Delivery(String description) {
+//        this.description = description;
+//    }
+
+    FAST, STANDARD;
+}
---
+++ spring-mvc2/form/src/main/java/hello/itemservice/enumPractise/Workspace.java
@@ -0,0 +1,11 @@
+package hello.itemservice.enumPractise;
+
+public class Workspace {
+
+    public static void main(String[] args) {
+        Delivery deliveryType = Delivery.FAST;
+
+        System.out.println("deliveryType = " + deliveryType);
+    }
+}
+
