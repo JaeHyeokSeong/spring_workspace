d5c2dc3fb51b666eef8be1b4f1a719f5a77642c0
JaeHyeokSeong <seong5763@gmail.com>
2024-04-24 02:10:59 +1000

    radio button

--- spring-mvc2/form/src/main/java/hello/itemservice/enumPractise/Workspace.java
+++ spring-mvc2/form/src/main/java/hello/itemservice/enumPractise/Workspace.java
@@ -1,11 +1,24 @@
 package hello.itemservice.enumPractise;
 
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemType;
+
 public class Workspace {
 
     public static void main(String[] args) {
-        Delivery deliveryType = Delivery.FAST;
+//        Delivery deliveryType = Delivery.FAST;
+//
+//        System.out.println("deliveryType = " + deliveryType);
 
-        System.out.println("deliveryType = " + deliveryType);
+        Item item = new Item();
+        item.setItemType(ItemType.BOOK);
+        System.out.println(item.getItemType());
+
+        ItemType[] values = ItemType.values();
+        for (ItemType itemType : values) {
+            System.out.println("itemType = " + itemType);
+        }
+
     }
 }
 
--- spring-mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ spring-mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -30,6 +31,11 @@ public class FormItemController {
         return regions;
     }
 
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
+    }
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -40,7 +46,7 @@ public class FormItemController {
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
-        log.info("item={}", item.toString());
+        log.info("@GetMapping(\"/form/items/{itemId}/\") item={}", item.toString());
         model.addAttribute("item", item);
         return "form/item";
     }
@@ -49,14 +55,13 @@ public class FormItemController {
     public String addForm(Model model) {
         Item item = new Item();
         model.addAttribute("item", item);
-        log.info("item={}", item.toString());
+        log.info("@GetMapping(\"/form/items/add/\") item={}", item.toString());
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-        log.info("item.open={}", item.getOpen());
-        log.info("item.regions={}", item.getRegions());
+        log.info("@PostMapping(\"/form/items/add/\") item={}", item.toString());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -72,7 +77,7 @@ public class FormItemController {
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
-        log.info("item={}", item.toString());
+        log.info("@PostMapping(\"/form/items/{itemId}/edit\") item={}", item.toString());
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
