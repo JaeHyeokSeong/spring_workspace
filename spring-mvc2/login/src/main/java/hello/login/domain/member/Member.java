904929bed6e6f5f74a6520ae5b95b43a3c20007d
JaeHyeokSeong <seong5763@gmail.com>
2024-05-11 23:44:05 +1000

    회원 가입

---
+++ spring-mvc2/login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,26 @@
+package hello.login.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@ToString
+public class Member {
+    private Long id;
+    @NotEmpty
+    private String loginId;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+
+    public Member(String loginId, String name, String password) {
+        this.loginId = loginId;
+        this.name = name;
+        this.password = password;
+    }
+}
