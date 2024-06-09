a1bb1f60139d6c69d4cca35aa65666aa8e452846
JaeHyeokSeong <seong5763@gmail.com>
2024-06-09 15:48:14 +1000

    포맷터 - Formatter 만들기

---
+++ spring-mvc2/typeconverter/src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,30 @@
+package hello.typeconverter.formatter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter formatter = new MyNumberFormatter();
+
+    /**
+     * 문자 1,000을 1000L로 변환
+     */
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L);
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
