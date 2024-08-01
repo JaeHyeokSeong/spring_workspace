794819ad6004891128c0bbadc4f1a8b6e1e1902a
JaeHyeokSeong <seong5763@gmail.com>
2024-08-01 18:21:27 +1000

    JDK 동적 프록시 - 예제 코드

---
+++ spring-advanced/proxy/src/test/java/hello/proxy/jdkdynamic/code/TimeInvocationHandler.java
@@ -0,0 +1,33 @@
+package hello.proxy.jdkdynamic.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeInvocationHandler implements InvocationHandler {
+
+    private final Object target;
+
+    public TimeInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        // target 호출
+        Object result = method.invoke(target, args); // call method, target
+        // controller object, method meta info
+        // service object, method meta info
+        // repository object, method meta info
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
