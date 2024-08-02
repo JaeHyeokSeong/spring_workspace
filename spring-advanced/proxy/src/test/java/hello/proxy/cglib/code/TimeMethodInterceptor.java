b8c3523dbfd52f71ddb380028340d34c5672932b
JaeHyeokSeong <seong5763@gmail.com>
2024-08-03 04:57:22 +1000

    CGLIB - 예제 코드

---
+++ spring-advanced/proxy/src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -0,0 +1,33 @@
+package hello.proxy.cglib.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        // 성능 최적화X 위의 방식을 권장함
+        // Object result = method.invoke(target, objects);
+        // 성능 최적화 (권장)
+        Object result = methodProxy.invoke(target, objects);
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
